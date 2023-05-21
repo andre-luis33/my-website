de504ba1ad05a0c31c744e62fdfd901e864c4d9c
André <andreluismoura22@gmail.com>
2023-05-21 13:31:58 -0300

    rem: remove console log

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
    const ENVIROMENT = window.location.origin.includes('127.0.0.1') ? 'dev' : 'prod'
    const IS_MOBILE = window.innerWidth <= 768
 
-   console.log(ENVIROMENT);
 
    const headerMenu = document.querySelector('.header-menu')
    const btnMobile  = document.querySelector('#btn-mobile')
